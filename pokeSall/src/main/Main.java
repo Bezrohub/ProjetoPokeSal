92ad6ba0b8f2e105593183bf92f5eada3e4e321b
Maria <eduardaroc.3@gmail.com>
2026-09-23 10:38:16 -0300

    Refactor main method for cleaner structure
    CheckStyle

--- pokeSall/src/main/Main.java
+++ pokeSall/src/main/Main.java
@@ -1,4 +1,6 @@
 package main;
+
+import java.util.List;
 import java.util.Scanner;
 import pokeSall.Game;
 import pokeSall.Jogador;
@@ -7,52 +9,48 @@ import pokeSall.Treinador;
 import pokeSalls.BulbaSall;
 import pokeSalls.CharSall;
 import pokeSalls.SquirtSall;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner ler = new Scanner(System.in);
-        PokeSall pokeSall;
-        String nome;
-        String opcao;
-        Treinador jogador;
-        Game game;
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
+    System.out.println("Bem vindo ao jogo PokeSal!");
+    System.out.println("Digite o seu nome: ");
+    Treinador jogador;
+    String opcao;
+    do {
+      System.out.println("===========================");
+      System.out.println("Escolha o seu PokeSal!");
+      System.out.println("1- BulbaSal");
+      System.out.println("2- CharSal");
+      System.out.println("3- SquirtSal");
+      opcao = ler.nextLine();
+    } while (!(opcao.equals("1") || opcao.equals("2") || opcao.equals("3")));
+    String nome;
+    Game game;
+    nome = ler.nextLine();
+    PokeSall pokeSall;
+    switch (opcao) {
+      case "1":
+        pokeSall = new BulbaSall();
+        jogador = new Jogador(nome, pokeSall);
+        game = new Game(jogador);
+        game.rodarGame();
+        break;
+      case "2":
+        pokeSall = new CharSall();
+        jogador = new Jogador(nome, pokeSall);
+        game = new Game(jogador);
+        game.rodarGame();
+        break;
 
-        System.out.println("Bem vindo ao jogo PokeSal!");
-        System.out.println("Digite o seu nome: ");
-        nome = ler.nextLine();
-
-        do{
-            System.out.println("===========================");
-            System.out.println("Escolha o seu PokeSal!");
-            System.out.println("1- BulbaSal");
-            System.out.println("2- CharSal");
-            System.out.println("3- SquirtSal");
-            opcao = ler.nextLine();
-        }while(!(opcao.equals("1") || opcao.equals("2") || opcao.equals("3")));
-
-        switch (opcao) {
-            case "1":
-                pokeSall = new BulbaSall();
-                jogador = new Jogador(nome, pokeSall);
-                game = new Game(jogador);
-                game.rodarGame();
-                break;
-        
-            case "2":
-                pokeSall = new CharSall();
-                jogador = new Jogador(nome, pokeSall);
-                game = new Game(jogador);
-                game.rodarGame();
-                break;
-
-            case "3":
-                pokeSall = new SquirtSall();
-                jogador = new Jogador(nome, pokeSall);
-                game = new Game(jogador);
-                game.rodarGame();
-                break;
-        }
-       
+      case "3":
+        pokeSall = new SquirtSall();
+        jogador = new Jogador(nome, pokeSall);
+        game = new Game(jogador);
+        game.rodarGame();
+        break;
+      default:
+        break;
     }
+  }
 }
